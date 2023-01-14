14712335bb8aef302687e3228aa577670c3762bf
Neeraj Roy <neerajroy06502@gmail.com>
2023-01-14 21:12:52 +0530

    intial commit
    sab yaahse shuru hua hai mai hi bhawan hu

---
+++ sample.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+// #include<algorithms>
+using namespace std;
+
+int main()
+{
+
+    cout << "Hello Ass ashish" << endl;
+    cout<< "hue hue heu 0"<<endl;
+
+    return 0;
+}
